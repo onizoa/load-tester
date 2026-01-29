94971e6cec8261aa32a7421e6c698cd6b5aa844e
onizoa <zdancantos@gmail.com>
2026-01-29 16:42:00 -0500

    Add Firebase configuration to config.js

---
+++ components/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDhBtACQ9a9MxLKwRXMBE0tnsE1ZQvy2sY",
+  authDomain: "schedulertraininghub.firebaseapp.com",
+  projectId: "schedulertraininghub",
+  storageBucket: "schedulertraininghub.firebasestorage.app",
+  messagingSenderId: "142656614006",
+  appId: "1:142656614006:web:fa61e9711dac48dbc43957",
+  measurementId: "G-YGDGXRSHN6"
+};
